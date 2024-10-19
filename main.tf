2c2304db3a3d150e61d8b76c2701ac364c9a1c83
ajay <ajaykavitha213@gmail.com>
2024-10-19 16:03:13 +0530

    final changes

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
 # main.tf
 
 provider "aws" {
-  region = "us-west-2"     # Replace with your desired AWS region
+  region = "eu-north-1"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name  # Variable for the bucket name
-  acl    = "private"        # The access control list (ACL), here it's private
+  bucket = var.bucket_name  # Reference to the variable for bucket name
+  acl    = "private"
 }
 
-# Variables file
+# Declare the bucket_name variable
 variable "bucket_name" {
   description = "Name of the S3 bucket to create"
   type        = string
 }
 
-output "bucket_name" {
-  value       = aws_s3_bucket.my_bucket.bucket
-  description = "The name of the S3 bucket"
+# Declare the tag variable
+variable "tag" {
+  description = "Tag for the release"
+  type        = string
 }
